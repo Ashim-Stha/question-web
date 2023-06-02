fbe7de51c813097579d0b92ef750837b1d2a871a
Ashim-Stha <ashimshrestha2384@gmail.com>
2023-06-02 21:45:15 +0545

    beautify search_results

--- website/mysite/static/js/search.js
+++ website/mysite/static/js/search.js
@@ -1,19 +1,80 @@
+// // // const form = document.getElementById("form");
+
+// // // form.addEventListener("submit", function (event) {
+// // //   //   event.preventDefault(); // Prevents the form from submitting and refreshing the page
+
+// // //   const queryInput = document.getElementById("query");
+// // //   const query = queryInput.value;
+
+// // //   console.log("Search query:", query);
+// // //   //   queryInput.innerText = query;
+// // //   document.getElementsByName("q")[0].placeholder = query;
+// // //   //   console.log(queryInput);
+
+// // //   // const url = "https://example.com/search?q=" + encodeURIComponent(query);
+
+// // //   // window.location.href = url;
+
+// // //   // You can perform further actions with the query string, such as making an API request or manipulating the DOM.
+// // // });
+
+// // const form = document.getElementById("form");
+// // const searchInput = document.getElementById("query");
+
+// // form.addEventListener("submit", function (event) {
+// //   // event.preventDefault();
+
+// //   // Get the user input from the search bar
+// //   const userInput = searchInput.value;
+
+// //   // Update the placeholder with the user input
+// //   searchInput.placeholder = userInput;
+
+// //   // Clear the search bar
+// //   searchInput.value = "";
+
+// //   // Perform any further processing or submit the form
+// //   // ...
+// // });
+
+// const form = document.getElementById("form");
+
+// form.addEventListener("submit", function (event) {
+//   // event.preventDefault(); // Prevents the form from submitting and refreshing the page
+
+//   const queryInput = document.getElementById("query");
+//   const query = queryInput.value;
+
+//   console.log("Search query:", query);
+//   queryInput.value = query;
+//   document.getElementById("query").value = query;
+//   document.getElementById("query").setAttribute("value", query);
+//   return query;
+
+//   // const url = "https://example.com/search?q=" + encodeURIComponent(query);
+
+//   // window.location.href = url;
+
+//   // You can perform further actions with the query string, such as making an API request or manipulating the DOM.
+// });
+// document.getElementById("query").setAttribute("value", query);
 const form = document.getElementById("form");
 
 form.addEventListener("submit", function (event) {
-  //   event.preventDefault(); // Prevents the form from submitting and refreshing the page
+  // event.preventDefault(); // Prevents the form from submitting and refreshing the page
 
   const queryInput = document.getElementById("query");
   const query = queryInput.value;
 
   console.log("Search query:", query);
-  //   queryInput.innerText = query;
-  document.getElementsByName("q")[0].placeholder = query;
-  //   console.log(queryInput);
-
+  queryInput.value = query;
+  document.getElementById("query").setAttribute("value", query);
+  const a = query;
   // const url = "https://example.com/search?q=" + encodeURIComponent(query);
 
   // window.location.href = url;
 
   // You can perform further actions with the query string, such as making an API request or manipulating the DOM.
 });
+document.getElementById("query").setAttribute("value", a);
+document.getElementById("query").value = a;
